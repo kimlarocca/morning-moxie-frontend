91952ab4618b256dcf07defad3f1570aff88eee3
Kim LaRocca <kim@kimlarocca.com>
2024-11-22 13:28:08 -0500

    help page

--- pages/help.vue
+++ pages/help.vue
@@ -9,7 +9,11 @@
     <div class="container p-4">
       <h1 class="mb-3">Help Center</h1>
       <divider class="my-6 w-2" />
-      <p>Coming soon!</p>
+      <p class="mb-4">Our help center is coming soon!</p>
+      <p class="mb-4">
+        If you have questions or concerns in the mean time, please feel free to
+        <a href="mailto:hello@morningmoxie.io">contact us</a>.
+      </p>
     </div>
   </div>
 </template>
