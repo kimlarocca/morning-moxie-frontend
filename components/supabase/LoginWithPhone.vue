d653ce0038523a67459b71767fce2359d740565d
Kim LaRocca <kim@kimlarocca.com>
2024-12-23 15:43:33 -0500

    update phone numbers

--- components/supabase/LoginWithPhone.vue
+++ components/supabase/LoginWithPhone.vue
@@ -24,7 +24,7 @@
             >
               <template #value="slotProps">
                 <div v-if="slotProps.value" class="flex align-items-center">
-                  <div>
+                  <div style="margin-top: -4px">
                     {{ slotProps.value.flag }}
                   </div>
                 </div>
@@ -49,7 +49,7 @@
               v-model="phone"
               required
               aria-labelledby="phone"
-              :mask="`+${selectedCountry?.code} (999) 999-9999`"
+              :mask="`+${selectedCountry?.code} ${selectedCountry?.mask}`"
               placeholder="Phone Number"
               fluid
             />
@@ -119,81 +119,25 @@ const showOtp = ref(false)
 
 const formattedPhone = computed(() => phone.value.replace(/\D/g, ''))
 
-const selectedCountry = ref({ name: 'United States', code: '1', flag: '🇺🇸' })
+const selectedCountry = ref({
+  name: 'United States',
+  code: '1',
+  flag: '🇺🇸',
+  mask: '(999) 999-9999'
+})
 const countries = [
-  { name: 'United States', code: '1', flag: '🇺🇸' },
-  { name: 'Albania', code: '355', flag: '🇦🇱' },
-  { name: 'Andorra', code: '376', flag: '🇦🇩' },
-  { name: 'Austria', code: '43', flag: '🇦🇹' },
-  { name: 'Belarus', code: '375', flag: '🇧🇾' },
-  { name: 'Belgium', code: '32', flag: '🇧🇪' },
-  { name: 'Bolivia', code: '591', flag: '🇧🇴' },
-  { name: 'Bosnia and Herzegovina', code: '387', flag: '🇧🇦' },
-  { name: 'Bulgaria', code: '359', flag: '🇧🇬' },
-  { name: 'China', code: '86', flag: '🇨🇳' },
-  { name: 'Croatia', code: '385', flag: '🇭🇷' },
-  { name: 'Cyprus', code: '357', flag: '🇨🇾' },
-  { name: 'Czech Republic', code: '420', flag: '🇨🇿' },
-  { name: 'Denmark', code: '45', flag: '🇩🇰' },
-  { name: 'Ecuador', code: '593', flag: '🇪🇨' },
-  { name: 'Estonia', code: '372', flag: '🇪🇪' },
-  { name: 'Falkland Islands', code: '500', flag: '🇫🇰' },
-  { name: 'Finland', code: '358', flag: '🇫🇮' },
-  { name: 'France', code: '33', flag: '🇫🇷' },
-  { name: 'French Guiana', code: '594', flag: '🇬🇫' },
-  { name: 'Germany', code: '49', flag: '🇩🇪' },
-  { name: 'Greece', code: '30', flag: '🇬🇷' },
-  { name: 'Guyana', code: '592', flag: '🇬🇾' },
-  { name: 'Hong Kong', code: '852', flag: '🇭🇰' },
-  { name: 'Hungary', code: '36', flag: '🇭🇺' },
-  { name: 'Iceland', code: '354', flag: '🇮🇸' },
-  { name: 'India', code: '91', flag: '🇮🇳' },
-  { name: 'Ireland', code: '353', flag: '🇮🇪' },
-  { name: 'Italy', code: '39', flag: '🇮🇹' },
-  { name: 'Japan', code: '81', flag: '🇯🇵' },
-  { name: 'Laos', code: '856', flag: '🇱🇦' },
-  { name: 'Latvia', code: '371', flag: '🇱🇻' },
-  { name: 'Liechtenstein', code: '423', flag: '🇱🇮' },
-  { name: 'Lithuania', code: '370', flag: '🇱🇹' },
-  { name: 'Luxembourg', code: '352', flag: '🇱🇺' },
-  { name: 'Macau', code: '853', flag: '🇲🇴' },
-  { name: 'Malaysia', code: '60', flag: '🇲🇾' },
-  { name: 'Maldives', code: '960', flag: '🇲🇻' },
-  { name: 'Malta', code: '356', flag: '🇲🇹' },
-  { name: 'Mexico', code: '52', flag: '🇲🇽' },
-  { name: 'Moldova', code: '373', flag: '🇲🇩' },
-  { name: 'Monaco', code: '377', flag: '🇲🇨' },
-  { name: 'Montenegro', code: '382', flag: '🇲🇪' },
-  { name: 'Netherlands', code: '31', flag: '🇳🇱' },
-  { name: 'New Zealand', code: '64', flag: '🇳🇿' },
-  { name: 'North Macedonia', code: '389', flag: '🇲🇰' },
-  { name: 'Norway', code: '47', flag: '🇳🇴' },
-  { name: 'Paraguay', code: '595', flag: '🇵🇾' },
-  { name: 'Peru', code: '51', flag: '🇵🇪' },
-  { name: 'Philippines', code: '63', flag: '🇵🇭' },
-  { name: 'Poland', code: '48', flag: '🇵🇱' },
-  { name: 'Portugal', code: '351', flag: '🇵🇹' },
-  { name: 'Puerto Rico', code: '1787', flag: '🇵🇷' },
-  { name: 'Romania', code: '40', flag: '🇷🇴' },
-  { name: 'Russia', code: '7', flag: '🇷🇺' },
-  { name: 'San Marino', code: '378', flag: '🇸🇲' },
-  { name: 'Serbia', code: '381', flag: '🇷🇸' },
-  { name: 'Singapore', code: '65', flag: '🇸🇬' },
-  { name: 'Slovakia', code: '421', flag: '🇸🇰' },
-  { name: 'Slovenia', code: '386', flag: '🇸🇮' },
-  { name: 'South Africa', code: '27', flag: '🇿🇦' },
-  { name: 'Spain', code: '34', flag: '🇪🇸' },
-  { name: 'Suriname', code: '597', flag: '🇸🇷' },
-  { name: 'Sweden', code: '46', flag: '🇸🇪' },
-  { name: 'Switzerland', code: '41', flag: '🇨🇭' },
-  { name: 'Taiwan', code: '886', flag: '🇹🇼' },
-  { name: 'Thailand', code: '66', flag: '🇹🇭' },
-  { name: 'Turkiye', code: '90', flag: '🇹🇷' },
-  { name: 'Ukraine', code: '380', flag: '🇺🇦' },
-  { name: 'United Kingdom', code: '44', flag: '🇬🇧' },
-  { name: 'Uruguay', code: '598', flag: '🇺🇾' },
-  { name: 'Vatican City', code: '379', flag: '🇻🇦' },
-  { name: 'Venezuela', code: '58', flag: '🇻🇪' }
+  { name: 'Australia', code: '61', flag: '🇦🇺', mask: '(99) 9999-9999' },
+  { name: 'Belgium', code: '32', flag: '🇧🇪', mask: '(999) 999-999' },
+  { name: 'China', code: '86', flag: '🇨🇳', mask: '(999) 9999-9999' },
+  { name: 'Hong Kong', code: '852', flag: '🇭🇰', mask: '(999) 9999-9999' },
+  { name: 'Ireland', code: '353', flag: '🇮🇪', mask: '(999) 999-999' },
+  { name: 'Macau', code: '853', flag: '🇲🇴', mask: '(999) 9999-9999' },
+  { name: 'New Zealand', code: '64', flag: '🇳🇿', mask: '(99) 999-9999' },
+  { name: 'Puerto Rico', code: '1787', flag: '🇵🇷', mask: '(999) 999-9999' },
+  { name: 'Singapore', code: '65', flag: '🇸🇬', mask: '(99) 9999-9999' },
+  { name: 'Spain', code: '34', flag: '🇪🇸', mask: '(999) 999-999' },
+  { name: 'United Kingdom', code: '44', flag: '🇬🇧', mask: '(999) 999-9999' },
+  { name: 'United States', code: '1', flag: '🇺🇸', mask: '(999) 999-9999' }
 ]
 
 const signup = () => {
